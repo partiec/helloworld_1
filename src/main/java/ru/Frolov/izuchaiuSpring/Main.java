debed2ba9355da3372a5198c73b80f6304d09aa7
Alexey Frolov <frolov.git@mail.ru>
2024-01-12 00:23:40 +0300

    one more 3

--- src/main/java/ru/Frolov/izuchaiuSpring/Main.java
+++ src/main/java/ru/Frolov/izuchaiuSpring/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Add one more 'Hello world 2");
+        System.out.println("one more 'Hello world 3");
     }
 }
